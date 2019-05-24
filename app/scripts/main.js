e571201456feac86801e45e1e470661ebb579da3
Daniel Dąbrowski <elosik123@onet.pl>
2019-05-24 16:16:24 +0200

    Animations for hero

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,11 @@
 (function() {
 	let headerImage = new Image();
 	let loader = document.getElementById('loader');
+	let header = document.getElementById('hero');
 	headerImage.src = 'imgs/header.jpg';
 	headerImage.onload = () => {
 		window.scrollTo({ top: 0, behavior: 'smooth' });
 		loader.classList.add('done');
+		header.classList.add('show');
 	};
 })();
