bbb8ce8baa1a8761e450f9323aed58fefdbbafb8
Daniel Dąbrowski <elosik123@onet.pl>
2019-05-24 16:03:45 +0200

    Loader

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,9 @@
 (function() {
+	let headerImage = new Image();
+	let loader = document.getElementById('loader');
+	headerImage.src = 'imgs/header.jpg';
+	headerImage.onload = () => {
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+		loader.classList.add('done');
+	};
 })();
